2bbfb4addd292c6da2ef0137003cafff6720839d
Nattapon Howong <74097224+IxDraZil@users.noreply.github.com>
2022-01-23 22:58:01 +0700

    .

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -19,7 +19,7 @@ export default function LandingPage() {
             setError("")
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
-            navigate("/")
+            navigate("overview")
         } catch {
             setError('Failed to Login')
         }
